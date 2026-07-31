c9d7b68626285cd50309e01e8df0af5bc55de54a
Sophia Marielle C. Mendoza <sophiamarielle.mendoza-24@cpu.edu.ph>
2026-07-31 21:13:56 +0800

    feat: add mang tomas, add config for testing to unblock wasteland

--- src/main.js
+++ src/main.js
@@ -229,6 +229,7 @@ function startGame(initialScene, loadingOptions = {}) {
 
   if (!game) {
     game = new Phaser.Game(config);
+    window.phaserGame = game;
     sceneManager.bindGame(game);
     game.events.once("game-ready", () => {
       loadingScreen.hide();
